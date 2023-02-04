4781bd99807ffcd867fd606f86644e889b0d0931
Jose Balboa <joselimberg291197@gmail.com>
2023-02-03 16:08:25 -0800

    fixed styles, fixed nextjs template navbar

--- next-template/src/pages/_app.jsx
+++ next-template/src/pages/_app.jsx
@@ -4,12 +4,15 @@ import { AuthProvider } from "@/context/auth";
 import "@/styles/globals.css";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.css";
+import { SSRProvider } from "react-bootstrap";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider refetchInterval={5 * 60} refetchOnWindowFocus={true}>
       <AuthProvider>
-        <Component {...pageProps} />
+        <SSRProvider>
+          <Component {...pageProps} />
+        </SSRProvider>
       </AuthProvider>
     </SessionProvider>
   );
