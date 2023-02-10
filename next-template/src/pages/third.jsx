940bfdaf9e7b8761d01e57be305c350f56f14c80
Jose Balboa <joselimberg291197@gmail.com>
2023-02-09 16:54:39 -0800

    added apexcharts nextjs

--- next-template/src/pages/third.jsx
+++ next-template/src/pages/third.jsx
@@ -12,7 +12,7 @@ import {
 import { Bar, Line } from "react-chartjs-2";
 import faker from "faker";
 // import zoomPlugin from "chartjs-plugin-zoom";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 // const zoomPlugin = dynamic(() => import('chartjs-plugin-zoom'), {
 //   ssr: false,
 // })
@@ -31,6 +31,8 @@ ChartJS.register(
   Tooltip,
   Legend
 );
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const options = {
   responsive: true,
@@ -69,10 +71,56 @@ const ThirdPage = () => {
       <div className="row d-flex justify-content-center">
         <h1>Otra página más</h1>
         <div className="w-75">
-          <Bar options={options} data={data} />
+          {/* <Bar options={options} data={data} /> */}
         </div>
         <div className="w-75">
-          <Line options={options} data={data} />
+          <Chart
+            type="line"
+            series={[
+              {
+                name: "Almacén 1",
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 }))
+              },
+              {
+                name: "Almacén 2",
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 }))
+              },
+              {
+                name: "Almacén 3",
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 }))
+              },
+              {
+                name: "Almacén 4",
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 }))
+              },
+            ]}
+            options={{
+              chart: {
+                id: "apexchart-example",
+                zoom: {
+                  enabled: true,
+                  type: "x",
+                  autoScaleYaxis: false,
+                  zoomedArea: {
+                    fill: {
+                      color: "#90CAF9",
+                      opacity: 0.4,
+                    },
+                    stroke: {
+                      color: "#0D47A1",
+                      opacity: 0.4,
+                      width: 1,
+                    },
+                  },
+                },
+              },
+              xaxis: {
+                categories: labels,
+              },
+            }}
+            width={"100%"}
+            height={500}
+          />
         </div>
       </div>
     </GenericLayout>
